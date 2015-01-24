c14c462b232e988c82430e40aff3cedd4ecbce18
JjyKs <jyri.saukkonen@gmail.com>
2015-01-24 21:54:42 +0200

    Revert "VIIKKO2 VOI VITTU NYT OIKEESTI"
    This reverts commit 095e4eda3dcb484986123cb32b2a159aab092c18.

--- lib/Average_Rating.rb
+++ lib/Average_Rating.rb
@@ -1,5 +1,4 @@
 module AverageRating
-  extend ActiveSupport::Concern
   def average_rating
     score = self.ratings.count(:score)
     "Has #{score} #{"rating".pluralize(score)}, average #{self.ratings.average(:score)}"
