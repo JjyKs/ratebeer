5ff1d9deaca568e0ac21cb8cef5cad0adba99ba1
JjyKs <jyri.saukkonen@gmail.com>
2015-02-08 03:27:29 +0200

    Viikko 4 Final
    :dancer:

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 https://lit-anchorage-9660.herokuapp.com/breweries
-[![Build Status](https://travis-ci.org/JjyKs/ratebeer-public.png)](https://travis-ci.org/JjyKs/ratebeer-public)
+
+[![Build Status](https://api.travis-ci.org/JjyKs/ratebeer.png)](https://api.travis-ci.org/JjyKs/ratebeer)
+
+[![Code Climate](https://codeclimate.com/github//JjyKs/ratebeer.png)](https://codeclimate.com/github/JjyKs/ratebeer)
--- spec/features/beers_spec.rb
+++ spec/features/beers_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 describe "Beer" do
+  let!(:user) { FactoryGirl.create :user }
+
   before :each do
+    sign_in(username:"Pekka", password:"Foobar1")
     @breweries = ["Koff", "Karjala", "Schlenkerla"]
     year = 1896
     @breweries.each do |brewery_name|
-      FactoryGirl.create(:brewery, name: brewery_name, year: year += 1)
+    FactoryGirl.create(:brewery, name: brewery_name, year: year += 1)
     end
   end
   it "with a proper name gets added" do
